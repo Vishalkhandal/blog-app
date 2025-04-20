beeafd5ce95e315031208b50b1935e8dee483c2e
vishalkhandal <vishalkhandal96@gmail.com>
2025-04-20 19:04:54 +0530

    add admin dashboard

---
+++ src/components/admin/AdminManu.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { FiCalendar, FiSearch } from 'react-icons/fi'
+import UserAccount from './UserAccount'
+
+
+function AdminManu() {
+    return (
+        <div className="border-b px-4 mb-4 pb-4 border-stone-200">
+            <div className="flex items-center justify-between p-0.5">
+                <div>
+                    <span className="text-sm font-bold block">🚀 Good morning, Tom!</span>
+                    <span className="text-xs block text-stone-500">
+                        Tuesday, Aug 8th 2023
+                    </span>
+                </div>
+
+                <form className="flex text-sm items-center gap-2 bg-stone-100 transition-colors hover:bg-violet-100 hover:text-violet-700 px-3 py-1.5 rounded">
+                    <input className='w-2xs' type="text" />
+                    <button type='submit'><FiSearch /></button>
+                </form>
+                
+                <UserAccount />
+            </div>
+        </div>
+    )
+}
+
+export default AdminManu
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -8,7 +8,7 @@ function MainLayout() {
     return (
         <div className='flex flex-col min-h-screen'>
             <Header />
-            <main className='flex-1 container mx-auto px-4 pyy-6'>
+            <main className='container mx-auto px-4 pyy-6'>
                 <Outlet />
             </main>
             <Footer />
