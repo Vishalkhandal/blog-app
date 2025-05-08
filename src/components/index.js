1153976ecc88d05007223a09458f2b1a17a82faa
vishalkhandal <vishalkhandal96@gmail.com>
2025-05-08 17:36:22 +0530

    add more ui components

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import Select from "./ui_components/Select";
+import Container from "./ui_components/container/Container";
+import Logo from "./ui_components/Logo";
+import LogoutBtn from "./ui_components/LogoutBtn";
+import RTE from "./ui_components/RTE";
+import Signup from "./ui_components/Signup";
+import Login from "./ui_components/Login";
+import Button from "./ui_components/Button";
+import PostForm from "./ui_components/post-form/PostForm";
+import Input from "./ui_components/Input";
+import Card from "./Card"
+import Header from "./Header"
+import Footer from "./Footer"
+export {
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    Button,
+    PostForm,
+    Input,
+    Select,
+    Card,
+    Header,
+    Footer
+}
--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Outlet } from 'react-router'
-import Header from '../components/Header'
+import { Header } from '../components'
+
 
 function AuthLayout() {
     return (
         <>
-            <Header />
+            <Header />            
             <div className="min-h-screen flex items-center justify-center">
                 <div className="w-full max-w-md bg-white shadow-lg rounded-xl p-8">
                     <Outlet />
